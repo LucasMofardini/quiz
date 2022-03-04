09e589fb072528c51205ebf6a66f6fecde874ff5
Lucas Mofardini <lucasmofardini@gmail.com>
2022-03-04 20:20:20 -0300

    Comecei a logica da troca de  alternativas no hook

--- src/quiz.js
+++ src/quiz.js
@@ -9,7 +9,7 @@ export default function Quiz() {
             indexRespostaCerta: 1,
             respostaCerta: "2",
             respostas: [
-                "TESTESTESTE", "2", "3", "4",
+                "Olá. tudo bem como voce está? estou bem e com voce aaaa eu preciso falar mais alguns caracteres", "2", "3", "4",
             ]
         },
         {
@@ -20,33 +20,34 @@ export default function Quiz() {
                 "1", "2", "3", "4",
             ]
         },
-        {
-            titulo: "10 + 10",
-            indexRespostaCerta: 2,
-            respostaCerta: "20",
-            respostas: [
-                "11", "12", "20", "34",
-            ]
-        },
+        // {
+        //     titulo: "10 + 10",
+        //     indexRespostaCerta: 2,
+        //     respostaCerta: "20",
+        //     respostas: [
+        //         "11", "12", "20", "34",
+        //     ]
+        // },
 
     ];
-    const inicioResposta = {
-        respostaCerta: 0,
-        respostaErrada: 0
+    const inicioAlternativa = {
+        alternativaUsuario: []
     }
-    const [resposta, setResposta] = useState(inicioResposta);
+    const [alternativa, setAlternativa] = useState(inicioAlternativa);
     const clicarNaResposta = (index, indexRespostaCerta,) => {
-        // if (index == indexRespostaCerta) {
-        //     // Resposta Certa
-        //     console.log("resposta certa")
-        // }
+        if (index == indexRespostaCerta) {
+            // Resposta Certa
+            console.log("resposta certa")
+        }
 
     }
     const clicouNaResposta = () => {
         /* Quando a pessoa Clicar no botao da resposta certa */
+
     }
-    const teste = () => {
+    const onChanceValue = (e) => {
         /* Funcao teste para entender o onchange */
+        console.log(e.target)
     }
     return (
         <section id="section-quiz">
@@ -59,28 +60,31 @@ export default function Quiz() {
                     {perguntas.map((pergunta, indexPai) => {
                         return (
                             <div key={indexPai}>
+                                {indexPai + 1}
                                 <div className='item-pergunta'>
                                     <h2 key={indexPai} className={indexPai}>{pergunta.titulo}</h2>
+
                                 </div>
                                 <div className='container-repostas'>
-                                    <div className='item-resposta'>
-                                        <ul className='box-respostas' onChange={teste}>
-                                            {pergunta.respostas.map((resposta, index) => {
-                                                return (
-                                                    <li>
-                                                        <input value={resposta} name={indexPai} type="radio" id={indexPai + index} key={index} className={index == pergunta.indexRespostaCerta ? "certa" : undefined}
-                                                            onClick={(e) => {
+                                    <ul className='box-respostas'>
+                                        {pergunta.respostas.map((alternativa, index) => {
+                                            return (
+                                                <div onChange={onChanceValue} >
+                                                    <li key={index} className={`${indexPai} | ${index}`}>
+                                                        <input value={alternativa} name={indexPai} type="radio" id={`${index} | ${indexPai}`} className={index == pergunta.indexRespostaCerta ? "certa" : undefined}
+                                                            onClick={() => {
                                                                 clicarNaResposta(index, pergunta.indexRespostaCerta);
                                                             }}
                                                         />
-                                                        <label for={indexPai + index}>{resposta}</label>
+                                                        <label htmlFor={`${index} | ${indexPai}`}>{alternativa}</label>
                                                     </li>
-                                                )
-                                            })}
-                                        </ul>
-                                        <button className="btn-responder" onClick={clicouNaResposta}>Reponder</button>
+                                                </div>
 
-                                    </div>
+                                            )
+                                        })}
+                                    </ul>
+                                    <button className="btn-responder" onClick={clicouNaResposta}>Reponder</button>
+
                                 </div>
                             </div>
                         );
